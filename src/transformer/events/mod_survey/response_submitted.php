182a8a4c4eef6765c9f2441cb57e7eda4f61477b
apui-avignon <86233315+apui-avignon@users.noreply.github.com>
2023-12-12 15:00:19 +0100

    Update response_submitted.php
    Update comments

--- src/transformer/events/mod_survey/response_submitted.php
+++ src/transformer/events/mod_survey/response_submitted.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Transform for the data record created event.
+ * Transform for the survey response submitted event.
  *
  * @package   logstore_xapi
  * @copyright Jerret Fowler <jerrett.fowler@gmail.com>
@@ -29,7 +29,7 @@ namespace src\transformer\events\mod_survey;
 use src\transformer\utils as utils;
 
 /**
- * Transformer for surver reponse submitted event.
+ * Transformer for survey reponse submitted event.
  *
  * @param array $config The transformer config settings.
  * @param \stdClass $event The event to be transformed.
