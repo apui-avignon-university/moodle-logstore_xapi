b86f84d2e6b43835fdb4ca5cf91c0ae791bceb84
apui-avignon <86233315+apui-avignon@users.noreply.github.com>
2023-12-12 15:04:16 +0100

    Update submission_assessed.php
    Update comments

--- src/transformer/events/mod_workshop/submission_assessed.php
+++ src/transformer/events/mod_workshop/submission_assessed.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Transform for the data record created event.
+ * Transform for the workshop submission assessed event.
  *
  * @package   logstore_xapi
  * @copyright Jerret Fowler <jerrett.fowler@gmail.com>
@@ -29,7 +29,7 @@ namespace src\transformer\events\mod_workshop;
 use src\transformer\utils as utils;
 
 /**
- * Transformer for workshop submission created event.
+ * Transformer for workshop submission assessed event.
  *
  * @param array $config The transformer config settings.
  * @param \stdClass $event The event to be transformed.
