73f3a9a70a0fa7647e3de6c66fca2ef3d7d761e8
apui-avignon <86233315+apui-avignon@users.noreply.github.com>
2023-12-12 14:57:03 +0100

    Update course_module_viewed.php
    Update comments

--- src/transformer/events/mod_glossary/course_module_viewed.php
+++ src/transformer/events/mod_glossary/course_module_viewed.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Transform for the data record created event.
+ * Transform for the glossary module viewed event.
  *
  * @package   logstore_xapi
  * @copyright Jerret Fowler <jerrett.fowler@gmail.com>
@@ -29,7 +29,7 @@ namespace src\transformer\events\mod_glossary;
 use src\transformer\utils as utils;
 
 /**
- * Transformer for data record created event.
+ * Transformer for glossary module viewed event.
  *
  * @param array $config The transformer config settings.
  * @param \stdClass $event The event to be transformed.
